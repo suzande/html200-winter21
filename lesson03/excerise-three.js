cae11778d926401c2f0f0f98e5abead359dc07e0
Suzanne DePoe <suzannedepoe@Suzannes-MacBook-Air.local>
2021-01-30 22:02:04 -0800

    added exercise-three.js

---
+++ lesson03/excerise-three.js
@@ -0,0 +1,10 @@
+//Lesson 03 - Regular Challenge 3
+
+//Create an array that has at least 5 strings:
+//Use a for loop to console log all of the elements plus the index and a comma
+
+fruit = ['apples', 'oranges', 'grapes', 'bananas', 'cherries']
+
+for(let a = 0; a < fruit.length; a++) {
+  console.log(a, fruit[a]);
+}
