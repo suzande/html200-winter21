91d2ae5e5580f114757d8b6d95113239286816df
Suzanne DePoe <suzannedepoe@Suzannes-MacBook-Air.local>
2021-01-30 21:47:50 -0800

    added exerciste 2

---
+++ lesson03/exercist-two.js
@@ -0,0 +1,9 @@
+//Lesson 03, Regular Challenge 2
+
+//Set a string variable to be a sentence of at least five words and a period.
+let today = "Today is a beautiful day for a walk.";
+let sentArr = today.split(' ');
+console.log(sentArr);
+
+//Make an array from that sentence and console.log the third word.
+console.log(sentArr[2]);
