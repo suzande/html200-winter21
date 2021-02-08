224c78283a76500be8eb2c842da689a5d8e643a5
Suzanne DePoe <suzannedepoe@Suzannes-MacBook-Air.local>
2021-02-07 22:11:23 -0800

    updated const

--- lesson03/exercise-one.js
+++ lesson03/exercise-one.js
@@ -1,7 +1,7 @@
 //Lesson-03, Regular Challenge 1
 
 //Create an array, with at least 5 strings.
-fruit = ['apples', 'oranges', 'grapes', 'cherries', 'kiwi'];
+const fruit = ['apples', 'oranges', 'grapes', 'cherries', 'kiwi'];
 
 //Add another element to the array. 
 fruit.push('lemon');
@@ -10,14 +10,14 @@ fruit.pop(3);
 console.log(fruit);
 
 //Remove the third element.
-fruit = ['apples', 'oranges', 'grapes', 'cherries', 'kiwi'];
+const fruit = ['apples', 'oranges', 'grapes', 'cherries', 'kiwi'];
 fruit.push('lemon');
 console.log(fruit);
 fruit.splice(2, 1);
 console.log(fruit);
 
 //Create a string from the elements and comma separate them.
-fruit = ['apples', 'oranges', 'grapes', 'cherries', 'kiwi'];
+const fruit = ['apples', 'oranges', 'grapes', 'cherries', 'kiwi'];
 
 let newFruitList = fruit.join(', ');
 console.log(newFruitList);
